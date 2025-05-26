ba55d1c66f02cefa28095c98530ae05737ead7e1
Luiza Hackenhar Naziazeno <142232479+luizahackenhaarnaziazeno@users.noreply.github.com>
2025-05-26 12:54:34 -0300

    arrumando

--- Grafos.java
+++ Grafos.java
@@ -8,8 +8,17 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.Queue;
+import java.util.LinkedList;
+
+
 
 public class Grafos {
+
+    private DigrafoValorado grafo;
+    private boolean[] visitados;
+    private int[] anteriores;
+    private int[] custo;
      public static void main(String[] args) {
         final String ANSI_PURPLE = "\u001B[35m";
         final String ANSI_RED = "\u001B[31m";
@@ -77,6 +86,7 @@ public class Grafos {
 
         scanner.close();
     }
+    
     public static void processarArquivo(String caminhoArquivo) throws IOException {
         String pasta = "C:\\projects\\Facul\\T2 Jb\\Nos-Passos-Dos-Gigantes\\casosdeteste\\"; // Defina o nome da pasta onde estão os arquivos
         BufferedReader reader = new BufferedReader(new FileReader(pasta + caminhoArquivo));
@@ -86,4 +96,47 @@ public class Grafos {
         }
         reader.close();
     }
+
+
+    public void Bfs(DigrafoValorado grafo, int origem){
+        final String ANSI_PURPLE = "\u001B[35m";
+        final String ANSI_RED = "\u001B[31m";
+        final String ANSI_GREEN = "\u001B[32m";
+        final String ANSI_CYAN = "\u001B[36m";
+        final String ANSI_BLUE = "\u001B[34m";
+
+        this.grafo = grafo;
+        visitados = new boolean[grafo.getNumVertices()];
+        anteriores = new int[grafo.getNumVertices()];
+        custo = new int[grafo.getNumVertices()];
+        for (int v = 0; v < anteriores.length; v++) {
+            anteriores[v] = -1;
+        }
+
+        Queue<Integer> fila = new LinkedList<>();
+        fila.add(origem);
+        while (!fila.isEmpty()) {
+            int v = fila.poll();
+            visitados[v] = true;
+            for(DigrafoValorado.Aresta aresta: grafo.adjacentes(v)) {
+                if(!visitados[aresta.w]) {
+                    fila.add(aresta.w);
+                    anteriores[aresta.w] = v;
+                    custo[aresta.w] = custo[v] + aresta.peso;
+                    visitados[aresta.w] = true;
+                }
+            }
+        }
+
+        System.out.println("v  visitados  anteriores");
+        for (int v = 0; v < grafo.getNumVertices(); v++) {
+            System.out.println(
+                ANSI_PURPLE + v + "  " +
+                (visitados[v] ? ANSI_GREEN + visitados[v] : ANSI_RED + visitados[v]) + ANSI_PURPLE + "  " +
+                ANSI_BLUE + anteriores[v] + ANSI_PURPLE + "  " +
+                ANSI_CYAN + custo[v] + ANSI_PURPLE
+            );
+        }
+    }
+    
 }
